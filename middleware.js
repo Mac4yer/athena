a4073fad3d4654badfa942a2ce402d9cf7379473
Mac4yer <meen26022547@gmail.com>
2024-05-02 22:26:31 +0700

    My first commit

--- middleware.js
+++ middleware.js
@@ -1,9 +1,9 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-  ignoredRoutes: ["/api/webhooks"],
+export default clerkMiddleware({
+  ignoredRoutes: ["/api/webhooks(.*)"],
 });
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
