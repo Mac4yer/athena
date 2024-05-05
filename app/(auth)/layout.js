611c82d5a95ea8012fe4d32717446cb71b7588f8
Mac4yer <meen26022547@gmail.com>
2024-05-05 19:32:16 +0700

    first my commit

--- app/(auth)/layout.js
+++ app/(auth)/layout.js
@@ -14,7 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${inter.className} bg-purple-2`}>{children}</body>
+        <body className={`${inter.className} bg-gradient-to-r bg-purple-2 p-4`}>
+          <div className="container mx-auto max-w-2xl">
+            {children}
+          </div>
+        </body>
       </html>
     </ClerkProvider>
   );
